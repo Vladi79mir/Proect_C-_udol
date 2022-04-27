3e2f864ea68acad48a93265a66f503da60bf98c7
Vladimir <anisin79@internet.ru>
2022-04-27 17:38:08 +0400

    proect 3.1 работа с генератором чисел

---
+++ proect_C#_3.1/Program.cs
@@ -0,0 +1,8 @@
+﻿// сложение двух чисел с использованием генератора случайных чисел
+int numberA = new Random().Next(1, 10);// 1,2,3,4,5,6,7,8,9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
+
