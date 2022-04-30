2b825bd4244a974c8e2de62498deebb7cf7893ba
Vladimir <anisin79@internet.ru>
2022-04-30 08:11:34 +0400

    proect_5 приветствие Маши

---
+++ proect_C#_5/Program.cs
@@ -0,0 +1,17 @@
+﻿// начало
+// ввести имя пользователя
+// если имя пользователя совпадает с "МАША" то поприветствовать по особенному
+// иначе поприветствовать по обычному
+// конец
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
